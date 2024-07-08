626d96bb8749297aee8fb14dea8b5fd4575bf3d9
reginsvillamor <regin.s.villamor@aexp.com>
2024-07-08 11:49:51 +0800

    update tehcyx/kind version

--- module/kind_cluster/outputs.tf
+++ module/kind_cluster/outputs.tf
@@ -1,3 +1,25 @@
+#kubeconfig - The kubeconfig for the cluster after it is created
+#client_certificate - Client certificate for authenticating to cluster.
+#client_key - Client key for authenticating to cluster.
+#cluster_ca_certificate - Client verifies the server certificate with this CA cert.
+#endpoint - Kubernetes APIServer endpoint.
+
+output "kubeconfig" {
+  value = kind_cluster.default.kubeconfig
+}
+
+output "client_certificate" {
+  value = kind_cluster.default.client_certificate
+}
+
+output "client_key" {
+  value = kind_cluster.default.client_key
+}
+
+output "cluster_ca_certificate" {
+  value = kind_cluster.default.cluster_ca_certificate
+}
+
 output "kubernetes_endpoint" {
   value = kind_cluster.default.endpoint
 }
--- module/kind_cluster/providers.tf
+++ module/kind_cluster/providers.tf
@@ -1,10 +1,11 @@
 terraform {
   required_providers {
     kind = {
+      # https://registry.terraform.io/providers/tehcyx/kind/latest/docs/resources/cluster
       source  = "tehcyx/kind"
-      version = "0.2.1"
+      version = "0.5.1"
     }
   }
 
-  required_version = ">= 1.0.0"
+#  required_version = ">= 1.0.0"
 }
